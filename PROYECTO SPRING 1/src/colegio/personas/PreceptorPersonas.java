158dac5ad9f4fc7c4858fbb7fea2de4cd6387279
LautaroVarela <lautaroxvarela@gmail.com>
2019-05-30 14:39:34 -0300

    Revert "Revert "Cmabios seguidos por pasos de tutoriales.""
    This reverts commit 3302829fcc8e540c8a824240b71be8de5ed07b72.

--- PROYECTO SPRING 1/src/colegio/personas/PreceptorPersonas.java
+++ PROYECTO SPRING 1/src/colegio/personas/PreceptorPersonas.java
@@ -1,8 +1,23 @@
 package colegio.personas;
 
 public class PreceptorPersonas implements Personas{
+	
+	private CreacionInformes informeNuevo;
+	
+	public PreceptorPersonas(CreacionInformes informeNuevo) {
+		this.informeNuevo = informeNuevo;
+	}
+	
+	
+	
 	public String getTareas() {
 		return "Gestionar datos de los alumnos";
+	}
+
+	
+	public String getInforme() {
+		
+		return "Este es el informe del Preceptor: " + informeNuevo.getInforme();
 	}
 
 }
